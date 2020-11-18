d2fc0fe3b1a2741de809fdad3cf6a2da66a1e7bb
jdpv01 <juandpv01@hotmail.com>
2020-11-18 14:20:30 -0500

    implemented adjacency list graph

--- src/graph/IGraph.java
+++
@@ -1,9 +0,0 @@
-package graph;
-
-public interface IGraph<T extends Comparable<T>, V extends Comparable<V>>{
-	public void addEdge(T vertex, T vertexEnd, boolean directed, double weight, V value);
-	public void removeEdge(T vertex, T vertexEnd, boolean directed, double weight);
-	public void addVertex(T valueVertex);
-	public void removeVertex(T valueVertex);
-	public int getNumVertex();
-}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 import model.Game;
 import util.SoundPlayer;
@@ -43,7 +44,7 @@ public class Main extends Application{
 			Scene scene= new Scene(root);
 			scene.setCursor(Cursor.cursor("https://icons.iconarchive.com/icons/imil/role-playing/32/Woman-3-icon.png"));
 			scene.getStylesheets().add(getClass().getResource("/resources/fontstyle.css").toExternalForm());
-			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			//primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
 			SoundPlayer.play("/sounds/intro.wav");
 			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("video-game.png")));
