93820eae9d9af9360e941625f35a5f4a2eaffaf6
Fernanda <FernandaRojas152@hotmail.com>
2020-11-26 16:37:09 -0500

    delete classes that weren't used and weren't planned to be use in the future

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Player{
+	private String nickname;
+	private Score score;
+	
+	public Player(String nickname, Score score) {
+		super();
+		this.nickname = nickname;
+		this.score = score;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public Score getScore() {
+		return score;
+	}
+
+	public void setScore(Score score) {
+		this.score = score;
+	}
+	
 	
 }
