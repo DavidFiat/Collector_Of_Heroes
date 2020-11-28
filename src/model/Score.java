7a1f0c7d1c43d2a0b421bba5bb553da9bcf00aa5
Fernanda <FernandaRojas152@hotmail.com>
2020-11-28 16:25:11 -0500

    black widow implemented

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,20 +1,30 @@
 package model;
 
-import stack.IStack;
+import java.time.LocalDate;
 
 public class Score {
-	private IStack<Integer> scores;
-
-	public Score() {
+	private int total;
+	private LocalDate date;
+	
+	public Score(int total, LocalDate date) {
+		this.total = total;
+		this.date = date;
 	}
 	
-	public IStack<Integer> getScores() {
-		return scores;
-	}
-
-	public void setScores(IStack<Integer> scores) {
-		this.scores = scores;
+	public int getTotal() {
+		return total;
 	}
 	
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 	
 }
