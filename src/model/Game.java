d8d3a2a72b5e4de1567b0326247f2756250c8eec
David <davidfiat24@gmail.com>
2020-12-05 14:33:47 -0500

    CHange

--- src/model/Game.java
+++ src/model/Game.java
@@ -112,7 +112,7 @@ public class Game implements Serializable {
 		return c;
 	}
 
-	public List<Vertex<Character>> getEnemyCharactersVetex() {
+	public List<Vertex<Character>> getEnemyCharactersVertex() {
 		return characters.getVertices();
 	}
 
